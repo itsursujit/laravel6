94a0f13d9fdfb94c799924b4ff4116eed98f0691
sujit <itsursujit@gmail.com>
2020-01-11 20:16:43 +0545

    Added Blog

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@
  */
 
 require('./bootstrap');
-import {TweenMax, CSSPlugin, ScrollToPlugin, Draggable, Elastic} from "gsap/all";
+
 !function (t) {
     "use strict";
     t('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
@@ -32,7 +32,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -40,6 +40,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+/*
 const app = new Vue({
     el: '#app',
 });
+*/
